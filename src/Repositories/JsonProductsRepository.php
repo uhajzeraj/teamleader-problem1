2768bdcdba0b57377fba809fd1ad78fbafe54378
Uran Hajzeraj <uhajzeraj@hotmail.com>
2021-11-20 23:54:29 +0100

    update product repository

--- src/Repositories/JsonProductsRepository.php
+++ src/Repositories/JsonProductsRepository.php
@@ -2,6 +2,8 @@
 
 namespace App\Repositories;
 
+use App\Models\Item;
+
 class JsonProductsRepository implements ProductsRepository
 {
     public function __construct(private string $rootDir)
@@ -13,5 +15,28 @@ class JsonProductsRepository implements ProductsRepository
         $products = json_decode(file_get_contents($this->rootDir . '/var/products.json'), true);
 
         return array_filter($products, fn ($product) => (int) $product['category'] === $categoryId);
+    }
+
+    public function getByIds(array $items): array
+    {
+        $products = json_decode(file_get_contents($this->rootDir . '/var/products.json'), true);
+
+        $itemIds = array_map(fn ($item) => $item['product-id'], $items);
+        $products = array_filter($products, fn ($product) => in_array($product['id'], $itemIds));
+
+        $result = [];
+
+        foreach ($products as $product) {
+            $item = array_values(array_filter($items, fn ($item) => $item['product-id'] === $product['id']))[0];
+
+            $result[] = new Item(
+                $product['id'],
+                $product['category'],
+                $product['price'] * 100,
+                $item['quantity'],
+            );
+        }
+
+        return $result;
     }
 }
--- src/Repositories/ProductsRepository.php
+++ src/Repositories/ProductsRepository.php
@@ -2,7 +2,14 @@
 
 namespace App\Repositories;
 
+use App\Models\Item;
+
 interface ProductsRepository
 {
     public function getByCategoryId(int $categoryId): array;
+
+    /**
+     * @return Item[]
+     */
+    public function getByIds(array $ids): array;
 }
