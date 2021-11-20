2208b7ad9773fab6cfcf92c63d5be8ae0536f53a
Uran Hajzeraj <uhajzeraj@hotmail.com>
2021-11-20 21:24:28 +0100

    Update dependencies.php

--- app/dependencies.php
+++ app/dependencies.php
@@ -2,15 +2,15 @@
 
 use App\Repositories\CustomerRepository;
 use App\Repositories\JsonCustomerRepository;
-use App\Repositories\JsonOrderRepository;
-use App\Repositories\OrderRepository;
+use App\Repositories\JsonProductsRepository;
+use App\Repositories\ProductsRepository;
 use DI\ContainerBuilder;
 use Psr\Container\ContainerInterface;
 
 return function (ContainerBuilder $containerBuilder) {
     $containerBuilder->addDefinitions([
         'root_dir' => fn () => dirname($_SERVER['DOCUMENT_ROOT']),
-        OrderRepository::class => fn (ContainerInterface $c) => new JsonOrderRepository($c->get('root_dir')),
+        ProductsRepository::class => fn (ContainerInterface $c) => new JsonProductsRepository($c->get('root_dir')),
         CustomerRepository::class => fn (ContainerInterface $c) => new JsonCustomerRepository($c->get('root_dir')),
     ]);
 };
