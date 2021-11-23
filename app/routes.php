0efa1b475e830b158ae251ce321f05c5da883861
Uran Hajzeraj <uhajzeraj@hotmail.com>
2021-11-23 20:56:43 +0100

    Update routes.php

--- app/routes.php
+++ app/routes.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-use App\Actions\HelloAction;
 use App\Actions\SaveOrderAction;
 use Slim\App;
 use Slim\Handlers\Strategies\RequestResponseArgs;
@@ -11,6 +10,5 @@ return function (App $app) {
     $routeCollector = $app->getRouteCollector();
     $routeCollector->setDefaultInvocationStrategy(new RequestResponseArgs());
 
-    $app->get('/hello/{name}', HelloAction::class);
     $app->post('/orders', SaveOrderAction::class);
 };
