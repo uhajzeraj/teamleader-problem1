52ac34e4490d9c5c4daf2330ee74552a1f842de9
Uran Hajzeraj <uhajzeraj@hotmail.com>
2021-11-23 20:35:52 +0100

    Update SwitchesCategoryDiscountHandler.php

--- src/Discounts/Handlers/SwitchesCategoryDiscountHandler.php
+++ src/Discounts/Handlers/SwitchesCategoryDiscountHandler.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Discounts\Handlers;
 
 use App\Discounts\Discount;
@@ -8,17 +10,29 @@ use App\Models\Order;
 
 class SwitchesCategoryDiscountHandler implements DiscountHandler
 {
+    private const FREE_ITEM_THRESHOLD = 5;
+
     public function handle(Order $order): void
     {
-        $items = array_filter($order->getItems(), fn (Item $item) => $item->getCategory() === Item::CATEGORY_SWITCHES);
+        $switchCategoryItems = array_filter(
+            $order->getItems(),
+            fn (Item $item) => $item->getCategory() === Item::CATEGORY_SWITCHES
+        );
+
+        $items = array_filter(
+            $switchCategoryItems,
+            fn (Item $item) => floor($item->getQuantity() / self::FREE_ITEM_THRESHOLD) > 0
+        );
 
         foreach ($items as $item) {
-            $freeItemsCount = floor($item->getQuantity() / 6);
+            $freeItemsCount = floor($item->getQuantity() / self::FREE_ITEM_THRESHOLD);
+
+            $item->increaseQuantity((int) $freeItemsCount);
 
             $itemDiscount = $item->getPrice() * $freeItemsCount;
 
             $order->applyDiscount(new Discount(
-                "sixth_switch_product_bought_{$item->getId()}",
+                "five_switch_products_bought_{$item->getId()}",
                 round($itemDiscount, 2),
             ));
         }
